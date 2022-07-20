d63293f20cda527a1fd92401e90f4cbe9d2c71d1
Aaron Moore <lakevna@gmail.com>
2022-07-20 22:47:04 +0100

    feat(life): CLI args with structopt

--- life/src/main.rs
+++ life/src/main.rs
@@ -2,13 +2,24 @@ mod generation;
 mod game;
 mod rule;
 
+use structopt::StructOpt;
 use generation::Generation;
 use game::Game;
 use rule::Rule;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name="life", about="A rust implementation of John Conway's Game of Life")]
+struct Opt {
+    #[structopt(default_value="200")]
+    width: usize,
+    #[structopt(default_value="100")]
+    height: usize,
+}
+
 fn main() {
+    let opt = Opt::from_args();
     let mut game = Game::new(
-        Generation::soup(200, 100),
+        Generation::soup(opt.width, opt.height),
         Rule::new("B3/S23"),
     );
     game.run();
